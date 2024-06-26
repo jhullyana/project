df24d5c9e2b6a2d9ad9ff15ebd4f16bd2d8554f6
jhullyana <jhullyana01@gmail.com>
2024-06-26 12:32:18 -0300

    Atualização de Eventos

--- src/Evento.java
+++ src/Evento.java
@@ -1,48 +1,91 @@
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Evento {
-
-    private int id;
-    private String nome;
-    private LocalDate data;
+    private String titulo;
     private String descricao;
-    private List<Participante> participantes;
+    private String data;
+    private String hora;
+    private String local;
+    private int capacidade;
+    private List<Participante> participantesInscritos;
 
-    public Evento(int id, String nome, LocalDate data, String descricao) {
-        this.id = id;
-        this.nome = nome;
-        this.data = data;
+    public Evento(String titulo, String descricao, String data, String hora,String local, int capacidade) {
+        this.titulo = titulo;
         this.descricao = descricao;
-        this.participantes = new ArrayList<>();
+        this.data = data;
+        this.hora = hora;
+        this.local = local;
+        this.capacidade = capacidade;
+        this.participantesInscritos = new ArrayList<>();
     }
 
     // Getters e Setters
 
-    public void adicionarParticipante(Participante participante) {
-        participantes.add(participante);
+    public String getTitulo() {
+        return titulo;
     }
 
-    public void setParticipantes(List<Participante> participantes) {
-        this.participantes = participantes;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public List<Participante> getParticipantesInscritos() {
+        return participantesInscritos;
+    }
+
+    public void inscreverParticipante(Participante participante) {
+        if (participantesInscritos.size() < capacidade) {
+            participantesInscritos.add(participante);
+        } else {
+            throw new RuntimeException("Capacidade máxima atingida");
+        }
     }
 
 
-    public List<Participante> getParticipantes() {
-        return participantes;
-    }
-
-    @Override
-    public String toString() {
-        return "Evento{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", data=" + data +
-                ", descricao='" + descricao + '\'' +
-                ", participantes=" + participantes +
-                '}';
+    public void setParticipantesInscritos(List<Participante> participantesInscritos) {
+        this.participantesInscritos = participantesInscritos;
     }
 }
-
